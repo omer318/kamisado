e9782239b3e4bc21db335111c3f7e63bd2b0b04a
mario318 <domer154@gmail.com>
2020-07-03 22:08:57 +0300

    added colors and keys

--- main_file.py
+++ main_file.py
@@ -4,19 +4,26 @@ from enum import Enum
 
 
 class Color(Enum):
-    RED = (0,0,255)
-    GREEN = (102,204,0)
+    WHITE = (0,0,0)
+    BROWN = (102,51,0)
+    GREEN = (50,204,0)
+    RED = (204,0,0)
+    YELLOW = (255,255,0)
+    PINK = (255,153,255)
+    PURPLE = (76,0,153)
     BLUE = (0,0,153)
+    ORANGE = (255,158,0)
 
 
 class Game_tile():
     def __init__(self,color):
         self.size = 64
         self.color = color
+        self.occupied = False
 
 def main():
-    setup_game()
-    loop_game()
+    screen = setup_game()
+    loop_game(screen)
     quit_game()
 
 
@@ -24,15 +31,28 @@ def setup_game():
     screen = pygame.display.set_mode((512,512))
     pygame.display.set_caption("Game")
 
-    screen.fill(Color.BLUE.value)
+    screen.fill(Color.WHITE.value)
     pygame.display.flip()
+    return screen
+
+def build_board():
+    pass
 
 
-def loop_game():
+def loop_game(screen):
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 quit_game()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_r:
+                    screen.fill(Color.RED.value)
+                if event.key == pygame.K_g:
+                    screen.fill(Color.GREEN.value)
+                if event.key == pygame.K_b:
+                    screen.fill(Color.BLUE.value)
+                pygame.display.flip()
 
 def quit_game():
     pygame.quit()
