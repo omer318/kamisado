fc350cc8365d50f23d1b5b39c1837c96967a944b
mario318 <domer154@gmail.com>
2020-07-04 15:57:33 +0300

    added all colors and made them cycle

--- main_file.py
+++ main_file.py
@@ -1,16 +1,17 @@
 import pygame
+import time
 
 from enum import Enum
 
 
 class Color(Enum):
-    WHITE = (0,0,0)
+    WHITE = (255,255,255)
     BROWN = (102,51,0)
-    GREEN = (50,204,0)
-    RED = (204,0,0)
-    YELLOW = (255,255,0)
-    PINK = (255,153,255)
-    PURPLE = (76,0,153)
+    GREEN = (50,175,0)
+    RED = (175,0,0)
+    YELLOW = (225,225,0)
+    PINK = (255,125,125)
+    PURPLE = (153,0,153)
     BLUE = (0,0,153)
     ORANGE = (255,158,0)
 
@@ -40,19 +41,18 @@ def build_board():
 
 
 def loop_game(screen):
-
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 quit_game()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_r:
-                    screen.fill(Color.RED.value)
-                if event.key == pygame.K_g:
-                    screen.fill(Color.GREEN.value)
-                if event.key == pygame.K_b:
-                    screen.fill(Color.BLUE.value)
                 pygame.display.flip()
+        for color in Color:
+            screen.fill(color.value)
+            pygame.display.flip()
+            time.sleep(0.5)
 
 def quit_game():
     pygame.quit()
